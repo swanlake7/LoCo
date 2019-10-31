d2d31e6ca038013959d5117111ac894424e97756
Bradley Pagano <bpagano5243@gmail.com>
2019-10-31 10:16:53 -0400

    adds working ajax resuest

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
+    let date = "october";
+    var cncrtLoctaion = "https://www.eventbriteapi.com/v3/events/search?token=UYZ2YFDWG4G7B3MB4SUQ&sort_by=date&q="+ date;
 
+$.ajax({
+    url: "https://cors-anywhere.herokuapp.com/" + cncrtLoctaion,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+});
 
 
 
