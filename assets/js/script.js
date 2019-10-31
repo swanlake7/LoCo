ca73811f632548e141521df459cb368747a34e99
Bradley Pagano <bpagano5243@gmail.com>
2019-10-31 14:25:29 -0400

    adds search string and object for Seat Geek

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
-$( document ).ready(function() {
-    let date = "october";
-    var cncrtLoctaion = "https://www.eventbriteapi.com/v3/events/search?token=UYZ2YFDWG4G7B3MB4SUQ&sort_by=date&q="+ date;
+$(document).ready(function () {
 
-$.ajax({
-    url: "https://cors-anywhere.herokuapp.com/" + cncrtLoctaion,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-});
+    $("#searchMe").on("click", function(e){
+        e.preventDefault();
+        var searchBy = $("#searchBy").val();
+        var custResponse = $("#searchTerm").val();
+        var searchObj = {};
+        searchObj[searchBy] = custResponse;
 
-
-
+        getEvents(searchObj);
+    })
 
 });
+
+function getEvents (searchParams) {
+    const searchString = $.param(searchParams);
+    var cncrtLoctaion = "https://api.seatgeek.com/2/events?client_id=OTA1MzgzM3wxNTcyNTMzMTQ3Ljk3&" + searchString;
+    $.ajax({
+        url: cncrtLoctaion,
+        method: "GET"
+    }).then(function (response) {
+        for (i = 0; i < response.events.length; i++) {
+            console.log(response.events[i].venue.location.lat);
+            console.log(response.events[i].venue.location.lon);
+        };
+    });
+}
