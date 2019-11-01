059f41612fbae35c558aac18acf8e2fcaae699e8
Bradley Pagano <bpagano5243@gmail.com>
2019-11-01 12:07:23 -0400

    adds text from JSON response to DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,11 @@ function getEvents (searchParams) {
         method: "GET"
     }).then(function (response) {
         for (i = 0; i < response.events.length; i++) {
+            $("#results").append("<div>").attr("class", "eventReturn");
+            $(".seatGeek").append("<p>Title: " + response.events[i].title + "</p>");
+            $(".seatGeek").append("<p>Type of Event: " + response.events[i].type + "</p>");
+            $(".seatGeek").append("<p>City: " + response.events[i].venue.city + "</p>");
+            $(".seatGeek").append("<p>Venue: " + response.events[i].venue.name + "</p>");
             console.log(response.events[i].venue.location.lat);
             console.log(response.events[i].venue.location.lon);
         };
--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,37 +1,37 @@
-let latitude = 37.8267;
-let longitude = -122.4233;
-let currently = {
-    time: "",
-    summary: "",
-    temperature: "",
-    humidity: "",
-    precipProbability: "",
-    nearestStormDistance: "",
-    icon: ""
-}
+// let latitude = 37.8267;
+// let longitude = -122.4233;
+// let currently = {
+//     time: "",
+//     summary: "",
+//     temperature: "",
+//     humidity: "",
+//     precipProbability: "",
+//     nearestStormDistance: "",
+//     icon: ""
+// }
 
-const queryURl = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/5f327f3818b345c19bad8b553c5e00de/" + latitude + "," + longitude
-console.log(queryURls)
+// const queryURl = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/5f327f3818b345c19bad8b553c5e00de/" + latitude + "," + longitude
+// console.log(queryURls)
 
-$.ajax({
-    url: queryURl,
-    method: "GET"
-}).then(function (response) {
-    console.log(response)
+// $.ajax({
+//     url: queryURl,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response)
 
-    function convertToF(celsius) {
-        var fahrenheit;
-        fahrenheit = (celsius * (9 / 5)) + 32;
-        return fahrenheit;
-    }
+//     function convertToF(celsius) {
+//         var fahrenheit;
+//         fahrenheit = (celsius * (9 / 5)) + 32;
+//         return fahrenheit;
+//     }
 
-    currently.temperature = convertToF(repsonse.currently.temperature);
-    currently.time = response.currently.time;
-    currently.summary = response.currently.summary;
-    currently.humidity = response.currently.humidity;
-    currently.precipProbability = response.currently.precipProbability;
-    currently.nearestStormDistance = response.currently.nearestStormDistance;
-    currently.icon = response.currently.icon;
+//     currently.temperature = convertToF(repsonse.currently.temperature);
+//     currently.time = response.currently.time;
+//     currently.summary = response.currently.summary;
+//     currently.humidity = response.currently.humidity;
+//     currently.precipProbability = response.currently.precipProbability;
+//     currently.nearestStormDistance = response.currently.nearestStormDistance;
+//     currently.icon = response.currently.icon;
 
-    console.log(currently)
-})
+//     console.log(currently)
+// })
