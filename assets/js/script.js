ce107e822bf422faefc8cb55dec4d38fdb426f18
Bradley Pagano <bpagano5243@gmail.com>
2019-11-01 14:10:16 -0400

    wraps weather.js in a function called by my ajax .then function. Also, changed the appending HTML tags to unordered list.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,34 @@
 $(document).ready(function () {
 
     $("#searchMe").on("click", function(e){
-        e.preventDefault();
-        var searchBy = $("#searchBy").val();
-        var custResponse = $("#searchTerm").val();
-        var searchObj = {};
-        searchObj[searchBy] = custResponse;
+        // e.preventDefault();
+        // var searchBy = $("#searchBy").val();
+        // var custResponse = $("#searchTerm").val();
+        // var searchObj = {};
+        // searchObj[searchBy] = custResponse;
 
-        getEvents(searchObj);
+        getEvents()//searchObj);
     })
 
 });
 
 function getEvents (searchParams) {
     const searchString = $.param(searchParams);
-    var cncrtLoctaion = "https://api.seatgeek.com/2/events?client_id=OTA1MzgzM3wxNTcyNTMzMTQ3Ljk3&" + searchString;
+    var cncrtLoctaion = "https://api.seatgeek.com/2/events?client_id=OTA1MzgzM3wxNTcyNTMzMTQ3Ljk3&q=mexico" + searchString;
     $.ajax({
         url: cncrtLoctaion,
         method: "GET"
     }).then(function (response) {
         for (i = 0; i < response.events.length; i++) {
-            $("#results").append("<div>").attr("class", "eventReturn");
-            $(".seatGeek").append("<p>Title: " + response.events[i].title + "</p>");
-            $(".seatGeek").append("<p>Type of Event: " + response.events[i].type + "</p>");
-            $(".seatGeek").append("<p>City: " + response.events[i].venue.city + "</p>");
-            $(".seatGeek").append("<p>Venue: " + response.events[i].venue.name + "</p>");
-            console.log(response.events[i].venue.location.lat);
-            console.log(response.events[i].venue.location.lon);
+            let eventLat = response.events[i].venue.location.lat;
+            let eventLon = response.events[i].venue.location.lon;
+            $("#results").append("<ul>").attr("class", "eventReturn");
+            $(".eventReturn").append("<li>Title: " + response.events[i].title + "</li>");
+            $(".eventReturn").append("<li>Type of Event: " + response.events[i].type + "</li>");
+            $(".eventReturn").append("<li>City: " + response.events[i].venue.city + "</li>");
+            $(".eventReturn").append("<li>Venue: " + response.events[i].venue.name + "</li>");
+            getWeather(eventLat, eventLon);
+
         };
     });
 }
